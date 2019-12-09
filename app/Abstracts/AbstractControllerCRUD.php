e4495c596106a8da09778a20a25e0d5e44872a82
Ikau <tlutru@gmail.com>
2019-12-09 11:33:38 +0100

    :sparkles: AbstractContronollerCRUD pour les modeles CRUD

---
+++ app/Abstracts/AbstractControllerCRUD.php
@@ -0,0 +1,65 @@
+<?php 
+
+namespace App\Abstracts;
+
+use Illuminate\Http\Request; 
+use Illuminate\Routing\Controller;
+
+abstract class AbstractControllerCRUD extends Controller
+{
+
+    /**
+     * Liste les donnees presentes dans la table du modele.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    abstract public function index();
+
+    /**
+     * Affiche le formulaire pour creer une nouvelle entre dans la table du modele.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    abstract public function create();
+
+    /**
+     * Sauvegarde une nouvelle entree dans la table du modele.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    abstract public function store(Request $request);
+
+    /**
+     * Affiche l'entree $id dans la table du modele.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    abstract public function show($id);
+
+    /**
+     * Affiche le formulaire d'edition de l'entree $id dans ma table du modele.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    abstract public function edit($id);
+
+    /**
+     * Met a jour l'entree $id dans la table du modele.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    abstract public function update(Request $request, $id);
+
+    /**
+     * Supprime l'entree $d de la table du modele.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    abstract public function destroy($id);
+}
