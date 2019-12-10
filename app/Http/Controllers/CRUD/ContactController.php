598d5b2d4eb0df23d2c50066c40c16b8e4c77e58
Ikau <tlutru@gmail.com>
2019-12-10 10:34:45 +0100

    :sparkles::poop: CRUD sans validation

--- app/Http/Controllers/CRUD/ContactController.php
+++ app/Http/Controllers/CRUD/ContactController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Abstracts\AbstractControllerCRUD;
 use App\Modeles\Contact;
+use App\Utils\Constantes;
 
 class ContactController extends AbstractControllerCRUD
 {
@@ -15,31 +16,36 @@ class ContactController extends AbstractControllerCRUD
 
         return view('contact.index', [
             'nombre'   => count($contacts),
-            'contacts' => $contacts
+            'contacts' => $contacts,
         ]);
     }
 
     public function create() 
     {
-        /*
-        $newContact = new Contact();
-
-        $newContact->nom = 'test';
-        $newContact->prenom = 'test';
-        $newContact->civilite = 'test';
-        $newContact->type = 'test';
-        $newContact->mail = 'test';
-        $newContact->telephone = 'test';
-        $newContact->adresse = 'test';
-
-        $newContact->save();
-        */
-
-        return view('contact.create');
+        return view('contact.form', [
+            'type'     => Constantes::TYPE_CONTACT,
+            'civilite' => Constantes::CIVILITE
+        ]);
     }
 
     public function store(Request $request) 
     {
+        $validation = $request->validate([
+            'nom'       => ['required', 'alpha_dash'],
+            'prenom'    => ['required', 'alpha_dash'],
+            'type'      => ['required', 'numeric'],
+            'mail'      => ['required', 'email'],
+
+            'civilite'  => ['nullable', 'numeric'],
+            'telephone' => ['nullable'],
+            'adresse'   => ['nullable'],
+        ]);
+
+        $requestContact = new Contact();
+        $requestContact->fill($request->all());
+        $requestContact->nullToDefault();
+
+        $requestContact->save();
         return redirect()->route('contacts.index');
     }
 
@@ -50,16 +56,33 @@ class ContactController extends AbstractControllerCRUD
 
     public function edit($id) 
     {
-        return view('contact.edit');
+        $contact = Contact::find($id);
+
+        return view('contact.form', [
+            'id'       => $id,
+            'contact'  => $contact,
+            'type'     => Constantes::TYPE_CONTACT,
+            'civilite' => Constantes::CIVILITE
+        ]);
     }
 
     public function update(Request $request, $id) 
     {
+        $contact = Contact::find($id);
+
+        $contact->fill($request->all());
+        $contact->nullToDefault();
+        $contact->save();
+
         return redirect()->route('contacts.index');
     }
 
     public function destroy($id) 
     {
+        Contact::destroy($id);
+
+        echo ("ok");
+
         return redirect()->route('contacts.index');
     }
 }
