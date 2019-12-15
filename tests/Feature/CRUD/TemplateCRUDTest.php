108523bf622d3e9cdf65754196fa60c69dd95ce5
Ikau <tlutru@gmail.com>
2019-12-15 16:44:32 +0100

    :sparkles: Template de test CRUD

---
+++ tests/Feature/CRUD/TemplateCRUDTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature\CRUD;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+/* Template a remplir
+use App\Modeles\Enseignant;
+use App\Http\Controllers\CRUD\EnseignantController;
+use App\Utils\Constantes;
+*/
+
+class TemplatetControllerTest extends TestCase
+{
+    // Rollback les modifications de la BDD a la fin des tests
+    use RefreshDatabase;
+
+    /**
+     * Attributs sur lesquels effectuer les tests
+     */
+    private $attributs = [
+    ];
+
+    /* ====================================================================
+     *                           TESTS AUXILIAIRES
+     * ====================================================================
+     */
+
+    public function testNormaliseOptionnels()
+    {
+        $this->assertTrue(TRUE);
+    }
+
+    /**
+     * @depends testNormaliseOptionnels
+     */
+    public function testValiderForm()
+    {
+        $this->assertTrue(TRUE);
+    }
+
+    public function testValiderModele()
+    {
+        $this->assertTrue(TRUE);
+    }
+
+    /* ====================================================================
+     *                           TESTS RESOURCES
+     * ====================================================================
+     */
+
+    public function testIndex()
+    {
+        $this->assertTrue(TRUE);
+    }
+
+    public function testCreate()
+    {
+        $this->assertTrue(TRUE);
+    }
+    
+    /**
+     * @depends testValiderForm
+     */
+    public function testStore()
+    {
+        $this->assertTrue(TRUE);
+    }
+
+    /**
+     * @depends testValiderModele
+     */
+    public function testShow()
+    {
+        $this->assertTrue(TRUE);
+    }
+
+    /**
+     * @depends testValiderForm
+     * @depends testValiderModele
+     */
+    public function testEdit()
+    {
+        $this->assertTrue(TRUE);
+    }
+
+    /**
+     * @depends testValiderForm
+     * @depends testValiderModele
+     */
+    public function testUpdate()
+    {
+        $this->assertTrue(TRUE);
+    }
+
+    /**
+     * @depends testValiderModele
+     */
+    public function testDestroy()
+    {
+        $this->assertTrue(TRUE);
+    }
+}
