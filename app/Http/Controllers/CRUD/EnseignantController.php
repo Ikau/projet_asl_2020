303c85c5efbd611dd4a2ae38f08c0f7d33f0723f
Ikau <tlutru@gmail.com>
2019-12-16 13:13:50 +0100

    :sparkles::white_check_mark: Methode 'EnseignantController->validerForm'

--- app/Http/Controllers/CRUD/EnseignantController.php
+++ app/Http/Controllers/CRUD/EnseignantController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\CRUD;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Validation\Rule;
 
 use App\Abstracts\AbstractControllerCRUD;
 use App\Modeles\Enseignant;
@@ -19,6 +20,12 @@ class EnseignantController extends AbstractControllerCRUD
     const TITRE_CREATE = 'Creer un enseignant';
     const TITRE_SHOW   = 'Details du enseignant';
     const TITRE_EDIT   = 'Editer un enseignant';
+
+
+    /* ====================================================================
+     *                             RESOURCES
+     * ====================================================================
+     */
 
     public function index()
     {
@@ -41,7 +48,7 @@ class EnseignantController extends AbstractControllerCRUD
 
     public function store(Request $request)
     {
-        //
+        $this->validerForm($request);
     }
 
     public function show($id)
@@ -66,15 +73,46 @@ class EnseignantController extends AbstractControllerCRUD
 
     public function tests(Request $request)
     {
-        return;
+        switch($request->test)
+        {
+            case 'normaliseInputsOptionnels':
+                $this->normaliseInputsOptionnels($request);
+                if(null === $request->stages) abort('404');
+                if(null === $request->soutenances_referent) abort('404');
+                if(null === $request->soutenances_candide ) abort('404');
+            return redirect('/');
+
+            case 'validerForm':
+                $this->validerForm($request);
+            return redirect('/');
+
+            default:
+                abort('404');
+            break;
+        }
     }
+
+    /* ====================================================================
+     *                             AUXILIAIRES
+     * ====================================================================
+     */
 
     /**
      * Fonction qui remplace les valeurs optionnels null par des valeurs par defaut
      */
     protected function normaliseInputsOptionnels(Request $request)
     {
-        return;
+        if(null === $request->stages
+        || ! $request->stages instanceof Collection)
+            $request->stages = new Collection();
+
+        if(null === $request->soutenances_referent
+        || ! $request->soutenances_referent instanceof Collection)
+            $request->soutenances_referent = new Collection();
+
+        if(null === $request->soutenances_candide
+        || ! $request->soutenances_candide instanceof Collection)
+            $request->soutenances_candide = new Collection();
     }
 
     /**
@@ -82,7 +120,27 @@ class EnseignantController extends AbstractControllerCRUD
      */
     protected function validerForm(Request $request)
     {
-        return;
+        $validation = $request->validate([
+            'nom'    => ['required', 'string'],
+            'prenom' => ['required', 'string'],
+            'email'  => ['required', 'email'],
+            'responsable_option' => [
+                'required', 
+                'integer',
+                Rule::in(array_merge(
+                    Constantes::OPTION['vide'],
+                    Constantes::OPTION['MRI'],
+                    Constantes::OPTION['STI'],
+                ))
+            ],
+            'responsable_departement' => [
+                'required', 
+                'integer',
+                Rule::in(Constantes::DEPARTEMENT),
+            ], 
+        ]);
+        
+        $this->normaliseInputsOptionnels($request);
     }
 
     /**
--- tests/Feature/CRUD/EnseignantCRUDTest.php
+++ tests/Feature/CRUD/EnseignantCRUDTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\CRUD;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Database\Eloquent\Collection;
 use Tests\TestCase;
 
 use App\Modeles\Enseignant;
@@ -34,14 +35,110 @@ class EnseignantControllerTest extends TestCase
      * ====================================================================
      */
 
-    public function testNormaliseOptionnels()
+     /**
+      * @dataProvider normaliseOptionnelsProvider
+      */
+    public function testNormaliseOptionnels($clefModifiee)
     {
-        $this->assertTrue(TRUE);
+        $donnee                = factory(Enseignant::class)->make()->toArray();
+        $donnee['test']        = 'normaliseInputsOptionnels';
+        $donnee[$clefModifiee] = null;
+
+        $response = $this->from(route('enseignants.tests'))
+        ->post(route('enseignants.tests'), $donnee)
+        ->assertRedirect('/');
     }
 
-    public function testValiderForm()
+    public function normaliseOptionnelsProvider()
     {
-        $this->assertTrue(TRUE);
+        return [
+          'Enseignant valide'         => ['aucune'] ,
+          'Soutenances candide null'  => ['soutenances_candide'],
+          'Soutenances referent null' => ['soutenances_referent'],
+          'Stages null'               => ['stages'],
+        ];
+    }
+
+    /**
+     * @dataProvider validerFormProvider
+     */
+    public function testValiderForm(array $donnee, string $routeAttendue)
+    {
+        $response = $this->from(route('enseignants.tests'))
+        ->post(route('enseignants.tests'), $donnee)
+        ->assertRedirect($routeAttendue);
+
+    }
+
+    public function validerFormProvider()
+    {
+        // 'Fix' pour utiliser les factories dans les providers
+        $this->refreshApplication();
+
+        // Rappel : les affectations par array sont des copies profondes
+        $enseignantValide = factory(Enseignant::class)->make()->toArray();
+        $enseignantValide['test'] = 'validerForm';
+
+        // Success
+        $stagesNull           = $enseignantValide;
+        $stagesInvalides      = $enseignantValide;
+        $soutenancesNull      = $enseignantValide;
+        $soutenancesInvalides = $enseignantValide;
+
+        $stagesNull['stages']      = null;
+        $stagesInvalides['stages'] = 'non_collection';
+        $soutenancesNull['soutenances_referent']      = null;
+        $soutenancesNull['soutenances_candide']       = null;
+        $soutenancesInvalides['soutenances_referent'] = 'non_collection';
+        $soutenancesInvalides['soutenances_candide']  = 'non_collection';
+
+        // Echecs
+        $nomNull         = $enseignantValide;
+        $prenomNull      = $enseignantValide;
+        $emailNull       = $enseignantValide;
+        $optionNull      = $enseignantValide;
+        $departementNull = $enseignantValide;
+
+        $nomNull['nom']                             = null;
+        $prenomNull['prenom']                       = null;
+        $emailNull['email']                         = null;
+        $optionNull['responsable_option']           = null;
+        $departementNull['responsable_departement'] = null;
+
+        $nomInvalide         = $enseignantValide;
+        $prenomInvalide      = $enseignantValide;
+        $emailInvalide       = $enseignantValide;
+        $optionInvalide      = $enseignantValide;
+        $departementInvalide = $enseignantValide;
+
+        $nomInvalide['nom']                             = 42;
+        $prenomInvalide['prenom']                       = 42;
+        $emailInvalide['email']                         = 'mauvaisEmail';
+        $optionInvalide['responsable_option']           = -1;
+        $departementInvalide['responsable_departement'] = -1;
+
+        // [array $donnee, string $routeAttendue]
+        return [
+            // Succes
+            'Enseignant valide'     => [$enseignantValide, '/'],
+            'Stages null'           => [$stagesNull, '/'],
+            'Stages invalides'      => [$stagesInvalides, '/'],
+            'Soutenances null'      => [$soutenancesNull, '/'],
+            'Soutenances invalides' => [$soutenancesInvalides, '/'],
+
+            // Echecs
+            'Nom null'         => [$nomNull, route('enseignants.tests')],
+            'Prenom null'      => [$prenomNull, route('enseignants.tests')],
+            'Email null'       => [$emailNull, route('enseignants.tests')],
+            'Option null'      => [$optionNull, route('enseignants.tests')],
+            'Departement null' => [$departementNull, route('enseignants.tests')],
+
+            'Nom invalide'         => [$nomInvalide, route('enseignants.tests')],
+            'Prenom invalide'      => [$prenomInvalide, route('enseignants.tests')],
+            'Email invalide'       => [$emailInvalide, route('enseignants.tests')],
+            'Option invalide'      => [$optionInvalide, route('enseignants.tests')],
+            'Departement invalide' => [$departementInvalide, route('enseignants.tests')],
+        ];
     }
 
     public function testValiderModele()
