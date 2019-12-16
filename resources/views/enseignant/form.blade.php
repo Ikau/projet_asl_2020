0a1a226eb3d37ebf6ec1f9349f15d8be4022ed4b
Ikau <tlutru@gmail.com>
2019-12-16 09:43:24 +0100

    :sparkles: Formulaire de creation d'un enseignant

---
+++ resources/views/enseignant/form.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('titre', $titre)
+
+@section('sidebar')
+@endsection
+
+@section('contenu')
+<div>
+    @if ( ! isset($enseignant) )
+    Formulaire de creation d'un enseignant
+    @else
+    Formulaire d'édition d'un enseignant
+    @endif
+</div>
+<div>
+    <a href="{{ route('enseignants.index') }}">Retour</a>
+</div>
+
+@if ( ! isset($enseignant) )
+<form method="POST" action="{{ route('enseignants.store') }}">
+@else
+<form method="POST" action="{{ route('enseignants.update', [$id ?? -1]) }}">
+@method('PATCH')
+@endif
+    @csrf
+
+    <label for="nom">Nom</label>
+    <input id="nom" name="nom" type="text" value="{{ $enseignant->nom ?? old('nom') }}">
+    @error('nom')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+    <br/>
+
+    <label for="prenom">Prenom</label>
+    <input id="prenom" name="prenom" type="text" value="{{ $enseignant->prenom ?? old('prenom') }}" >
+    @error('prenom')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+    <br/>
+
+    <label for="email">Mail</label>
+    <input id="email" name="email" type="text" value="{{ $enseignant->email ?? old('email') }}" >
+    @error('email')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+    <br/>
+
+    <label for="responsable_option">Responsable d'option ?</label>
+    <select name="responsable_option" id="responsable_option" value="{{ $enseignant->responsable_option ?? old('responsable_option') }}" >
+        @foreach($options as $nomOption => $option)
+        <optgroup label="{{ $nomOption }}">
+            @foreach($option as $key => $value)
+            <option value="{{ $value }}">{{ $key }}</option>
+            @endforeach
+        </optgroup>
+        @endforeach
+    </select>
+    @error('responsable_option')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+    <br/>
+
+    <label for="responsable_departement">Responsable de département ?</label>
+    <select name="responsable_departement" id="responsable_departement" value="{{ $enseignant->responsable_departement ?? old('responsable_departement') }}" >
+        @foreach($departements as $key => $value)
+        <option value="{{ $value }}">{{ $key }}</option>
+        @endforeach
+    </select>
+    @error('responsable_departement')
+    <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+    <br/>
+
+    @if ( ! isset($enseignant))
+    <button type="submit"> Créer le enseignant</button>
+    @else
+    <button type="submit"> Modifier le enseignant</button>
+    @endif
+</form>
+
+@endsection
