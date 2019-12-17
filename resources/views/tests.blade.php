e8845c993b714bdc8a2d5219f5d7087aa681a481
Ikau <tlutru@gmail.com>
2019-12-17 15:07:30 +0100

    :sparkles: Ajout route test vers EtudiantController

--- resources/views/tests.blade.php
+++ resources/views/tests.blade.php
@@ -5,4 +5,5 @@
 
     <a href="{{ route('contacts.index') }}">Contact</a>
     <a href="{{ route('enseignants.index') }}">Enseignant</a>
+    <a href="{{ route('etudiants.index') }}">Etudiant</a>
 </html>
