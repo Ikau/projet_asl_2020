315cc9c7ce620ccc5db88c400945ec1cfd2efc38
Ikau <tlutru@gmail.com>
2019-12-17 14:54:15 +0100

    :sparkles: Factory 'Etudiant'

--- database/factories/ModelesFactory.php
+++ database/factories/ModelesFactory.php
@@ -8,15 +8,17 @@ use Illuminate\Support\Str;
 
 use App\Modeles\Contact;
 use App\Modeles\Enseignant;
+use App\Modeles\Etudiant;
 use App\Modeles\Departement;
 use App\Modeles\Option;
+
 use App\Utils\Constantes;
 
 $factory->define(Contact::class, function (Faker $faker)
 {
     return [
-        'nom'       => $faker->firstName,
-        'prenom'    => $faker->lastName,
+        'nom'       => $faker->lastName,
+        'prenom'    => $faker->firstName,
         'civilite'  => $faker->randomElement(Constantes::CIVILITE),
         'type'      => $faker->randomElement(Constantes::TYPE_CONTACT),
         'email'     => $faker->unique()->safeEmail,
@@ -27,14 +29,30 @@ $factory->define(Contact::class, function (Faker $faker)
 
 $factory->define(Enseignant::class, function (Faker $faker)
 {
-    $idDepartements = DB::table(Departement::NOM_TABLE)->pluck('id');
-    $idOptions      = DB::table(Option::NOM_TABLE)->pluck('id');
+    $idsDepartement = DB::table(Departement::NOM_TABLE)->pluck('id');
+    $idsOption      = DB::table(Option::NOM_TABLE)->pluck('id');
 
     return [
-        Enseignant::COL_NOM                        => $faker->firstname,
-        Enseignant::COL_PRENOM                     => $faker->lastName,
+        Enseignant::COL_NOM                        => $faker->lastName,
+        Enseignant::COL_PRENOM                     => $faker->firstname,
         Enseignant::COL_EMAIL                      => $faker->unique()->safeEmail,
-        Enseignant::COL_RESPONSABLE_DEPARTEMENT_ID => $faker->randomElement($idDepartements),
-        Enseignant::COL_RESPONSABLE_OPTION_ID      => $faker->randomElement($idOptions)
+        Enseignant::COL_RESPONSABLE_DEPARTEMENT_ID => $faker->randomElement($idsDepartement),
+        Enseignant::COL_RESPONSABLE_OPTION_ID      => $faker->randomElement($idsOption)
+    ];
+});
+
+$factory->define(Etudiant::class, function (Faker $faker)
+{
+    $idsDepartement = DB::table(Departement::NOM_TABLE)->pluck('id');
+    $idsOption      = DB::table(Option::NOM_TABLE)->pluck('id');
+
+    return [
+        Etudiant::COL_NOM            => $faker->lastName,
+        Etudiant::COL_PRENOM         => $faker->firstName,
+        Etudiant::COL_EMAIL          => $faker->unique()->safeEmail,
+        Etudiant::COL_ANNEE          => RandomElement([4,5]),
+        Etudiant::COL_MOBILITE       => $faker->boolean(50),
+        Etudiant::COL_DEPARTEMENT_ID => $faker->randomElement($idsDepartement),
+        Etudiant::COL_OPTION_ID      => $faker->randomElement($idsOption)
     ];
 });
