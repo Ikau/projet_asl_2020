f8932299f4c3477722a28884926af8818876fdf0
Ikau <tlutru@gmail.com>
2019-12-18 23:57:14 +0100

    :truck: Renommage du dossier 'liste' en 'foreach'

--- resources/views/includes/foreach/departements.blade.php
+++ resources/views/includes/foreach/departements.blade.php
@@ -1,7 +1,7 @@
 {{-- 
     Liste les differentes options de l'INSA dans un tag <select>
 
-    Variables recues de la vue appelante :
+    Variables a definir depuis la vue appelante :
         'departements' : collection de App\Modeles\Departement
 --}}
 @foreach($departements as $departement)
