e94ace5c6bc7ed5c058559d397b6ce41742ab040
Ikau <tlutru@gmail.com>
2019-12-19 13:54:19 +0100

    :sparkles: Factory 'Stage'

--- database/factories/ModelesFactory.php
+++ database/factories/ModelesFactory.php
@@ -11,6 +11,7 @@ use App\Modeles\Enseignant;
 use App\Modeles\Etudiant;
 use App\Modeles\Departement;
 use App\Modeles\Option;
+use App\Modeles\Stage;
 
 use App\Utils\Constantes;
 
@@ -54,5 +55,32 @@ $factory->define(Etudiant::class, function (Faker $faker)
         Etudiant::COL_MOBILITE       => $faker->boolean(50),
         Etudiant::COL_DEPARTEMENT_ID => $faker->randomElement($idsDepartement),
         Etudiant::COL_OPTION_ID      => $faker->randomElement($idsOption)
+    ];
+});
+
+$factory->define(Stage::class, function (Faker $faker)
+{
+    $idsEnseignant = DB::table(Enseignant::NOM_TABLE)->pluck('id');
+    $idsEtudiant   = DB::table(Etudiant::NOM_TABLE)->pluck('id');
+
+    return [
+        Stage::COL_ANNEE              => $faker->randomElement([4, 5]),
+        Stage::COL_CONVENTION_ENVOYEE => $faker->boolean(50),
+        Stage::COL_CONVENTION_SIGNEE  => $faker->boolean(50),
+        Stage::COL_DATE_DEBUT         => $faker->date(),
+        Stage::COL_DATE_FIN           => $faker->date(),
+
+        //$nbMaxDecimals = NULL, $min = 0, $max = NULL
+        Stage::COL_DUREE_SEMAINES     => $faker->randomFloat(0, 16, 32),
+        Stage::COL_GRATIFICATION      => $faker->randomFloat(2, 0, 2000),
+
+        Stage::COL_INTITULE           => $faker->jobTitle,
+        Stage::COL_LIEU               => $faker->city,
+        Stage::COL_MOYEN_RECHERCHE    => $faker->domainName,
+        Stage::COL_RESUME             => $faker->text,
+        
+        // Clefs etrangeres
+        Stage::COL_REFERENT_ID   => $faker->randomElement($idsEnseignant),
+        Stage::COL_ETUDIANT_ID   => $faker->unique()->randomElement($idsEtudiant),
     ];
 });
