d9dae2959533afd46c0d3a0de8ccd858f88c9c1f
Ikau <tlutru@gmail.com>
2019-12-19 10:28:39 +0100

    :construction: Desactivation de l'entreprise

--- app/Abstracts/AbstractStage.php
+++ app/Abstracts/AbstractStage.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 abstract class AbstractStage extends Model
 {
-    abstract public function entreprise();
+    //abstract public function entreprise();
     abstract public function etudiant();
     //abstract public function fiche_entreprise();
     //abstract public function fiche_rapport();
