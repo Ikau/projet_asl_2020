a46b9d08e3304bf46d4adcaa17a65e4f6a6f4292
Ikau <tlutru@gmail.com>
2020-01-07 16:18:31 +0100

    :construction: Utilisateur 'admin' basique

---
+++ database/migrations/2020_01_07_144534_peupler_table_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
+
+class PeuplerTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Creation d'un compte basique
+        DB::table('users')->insert([
+            'name'     => 'admin',
+            'email'    => 'admin@insa-cvl.fr',
+            'password' => Hash::make('admin')
+        ]);
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::table('users')->delete();
+    }
+}
