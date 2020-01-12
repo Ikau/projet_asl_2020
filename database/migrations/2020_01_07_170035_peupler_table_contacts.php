c3a677f6c29d6ffa7e80cc819f8a1fc74ed66ff6
Ikau <tlutru@gmail.com>
2020-01-12 22:14:41 +0100

    :sparkles: Peuplement table 'contacts'

--- database/migrations/2020_01_07_170035_peupler_table_contacts.php
+++ database/migrations/2020_01_07_170035_peupler_table_contacts.php
@@ -22,12 +22,19 @@ class PeuplerTableContacts extends Migration
             Contact::COL_NOM       => 'admin',
             Contact::COL_PRENOM    => 'admin',
             Contact::COL_CIVILITE  => Constantes::CIVILITE['vide'],
-            Contact::COL_TYPE      => Constantes::TYPE_CONTACT['administration_insa'],
+            Contact::COL_TYPE      => Constantes::TYPE_CONTACT['insa'],
             Contact::COL_EMAIL     => 'admin@insa-cvl.fr',
             Contact::COL_TELEPHONE => '',
             Contact::COL_ADRESSE   => '',
         ]);
         $contactAdmin->save();
+
+        // Ajout de contacts aleatoires
+        $nbContact = 20;
+        for($i=0; $i<$nbContact; $i++)
+        {
+            factory(Contact::class)->create();
+        }
 
     }
 
