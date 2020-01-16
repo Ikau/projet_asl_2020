dce1c2616124f1e598e8fb59b38c4dd6a49c96b2
Ikau <tlutru@gmail.com>
2020-01-16 14:20:44 +0100

    :sparkles: Implementation interface 'Gates' pour controle acces 'enseignant'

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,13 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Access\Response;
 
-class AuthServiceProvider extends ServiceProvider
+use App\Interfaces\Gates;
+use App\Modeles\Privilege;
+use App\Http\Controllers\Enseignant\ReferentController;
+
+class AuthServiceProvider extends ServiceProvider implements Gates
 {
     /**
      * The policy mappings for the application.
@@ -25,6 +30,24 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Gates personnalises
+        $this->enregistrerGatesReferent();
+    }
+
+    /**
+     * Enregistre toutes les regles 'Gates' relatifs au controle d'acces pour l'espace.
+     * 
+     * @return void
+     */
+    public function enregistrerGatesReferent()
+    {
+        Gate::define(ReferentController::GATE_GET_ACCUEIL, function($user) {
+            // Recuperation privilege referent
+            $privilegeReferent = Privilege::where(Privilege::COL_INTITULE, '=', 'Referent')->first();
+
+            return $user->privileges()->get()->contains($privilegeReferent)
+                ? Response::allow()
+                : Response::deny('Seuls les enseignants autorises peuvent acceder a cette partie du site.');
+        });
     }
 }
