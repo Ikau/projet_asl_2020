6ba59b1eac113cf470630c1a2b0df2994913e869
Ikau <tlutru@gmail.com>
2020-01-20 10:33:55 +0100

    :construction::white_check_mark: Page 'affectation' referent

--- app/Abstracts/Controllers/Enseignant/AbstractReferentController.php
+++ app/Abstracts/Controllers/Enseignant/AbstractReferentController.php
@@ -12,4 +12,9 @@ abstract class AbstractReferentController extends Controller
      * Renvoie la page d'accueil de l'enseignant authentifie.
      */
     abstract public function index();
+
+    /**
+     * Renvoie la page contenant toutes les affectations de l'ensignant.
+     */
+    abstract public function affectations();
 }
