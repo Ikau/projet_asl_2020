e5dd43312712d796168eed4ce97a1aa5d425de5d
Ikau <tlutru@gmail.com>
2020-02-03 10:19:33 +0100

    :construction: Stub pour les vues de FicheRapportController

---
+++ resources/views/fiches/rapport/show.blade.php
@@ -0,0 +1,15 @@
+{{--
+    TEMPTEMPTEMPTEMPTEMPTEMPTEMP
+
+    Variables a definir depuis le controller appelant :
+        'titre' => string Le titre de l'onglet
+--}}
+@extends('layouts.app')
+
+@section('titre', $titre)
+
+
+@section('contenu')
+
+
+@endsection
