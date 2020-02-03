4a680096d4fe8f7e4a46837d77b9361ff68b707e
Ikau <tlutru@gmail.com>
2020-02-03 09:20:44 +0100

    :sparkles: Fichier de template vide pour les vues blade.php

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,13 @@
+{{--
+    TEMPTEMPTEMPTEMPTEMPTEMPTEMP
+
+    Variables a definir depuis le controller appelant :
+        'titre' => string Le titre de l'onglet
+--}}
+@extends('layouts.app')
+
+@section('titre', $titre)
+
+
+@section('contenu')
+@endsection
