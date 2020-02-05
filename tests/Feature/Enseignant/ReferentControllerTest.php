0dcc741f339bdccec152128f4daacde5d363da11
Ikau <tlutru@gmail.com>
2020-02-05 09:39:37 +0100

    :art: Refactorisation via trait 'TestAuthentification

--- tests/Feature/Enseignant/ReferentControllerTest.php
+++ tests/Feature/Enseignant/ReferentControllerTest.php
@@ -13,69 +13,17 @@ use App\Modeles\Enseignant;
 use App\Modeles\Role;
 use App\Http\Controllers\Enseignant\ReferentController;
 use App\Utils\Constantes;
+use Tests\Traits\TestAuthentification;
 
 class ReferentControllerTest extends TestCase
 {
     // Rollback les modifications de la BDD a la fin des tests
-    use RefreshDatabase;
+    use RefreshDatabase, TestAuthentification;
 
     /* ------------------------------------------------------------------
      *           AUXILIAIRES : tests de controle d'acces
      * ------------------------------------------------------------------
      */
-    /**
-     * Un utilisateur non authentifie doit etre renvoye a la page de login
-     * 
-     * @dataProvider controleAccesProvider
-     */
-    public function testNonAuth(string $uriRoute)
-    {
-        $this->assertGuest()
-        ->from('/')
-        ->get(route($uriRoute))
-        ->assertRedirect(route('login'));
-    }
-
-    /**
-     * Un utilisateur qui n'est pas de la classe App\Modeles\Enseignant n'est pas autorise
-     * 
-     * @dataProvider controleAccesProvider
-     */
-    public function testNonTypeEnseignant(string $uriRoute)
-    {
-        // Creation d'un utilisateur aleatoire
-        $contact = factory(Contact::class)->create();
-        $user    = factory(User::class)->make();
-        $user->identite()->associate($contact);
-        $user->save();
-
-        // Routage echec
-        $this->actingAs($user)
-        ->from('/')
-        ->get(route($uriRoute))
-        ->assertStatus(403);
-    }
-
-    /**
-     * Un utilisateur qui n'a pas le role App\Modeles\Role::VAL_ENSEIGNANT n'est pas autorise
-     * 
-     * @dataProvider controleAccesProvider
-     */
-    public function testNonRoleEnseignant(string $uriRoute)
-    {
-        // Creation d'un enseignant aleatoire
-        $enseignant = factory(Enseignant::class)->create();
-        $user       = factory(User::class)->make();
-        $user->identite()->associate($enseignant);
-        $user->save();
-
-        // Route echec
-        $this->actingAs($user)
-        ->from('/')
-        ->get(route($uriRoute))
-        ->assertStatus(403);
-    }
-
     /**
      * Permet de facilement tester les differentes routes
      */
@@ -97,7 +45,7 @@ class ReferentControllerTest extends TestCase
      * Test de feature pour acceder a l'accueil de l'espace 'enseignant'
      */
     public function testIndex()
-    {   
+    {
         // Creation d'un enseignant valide
         $user = $this->creerUserRoleEnseignant();
 
--- tests/Feature/Enseignant/ResponsableControllerTest.php
+++ tests/Feature/Enseignant/ResponsableControllerTest.php
@@ -16,68 +16,17 @@ use App\Modeles\Role;
 use App\Http\Controllers\Enseignant\ResponsableController;
 
 use App\Utils\Constantes;
+use Tests\Traits\TestAuthentification;
 
 class ResponsableControllerTest extends TestCase
 {
     // Rollback les modifications de la BDD a la fin des tests
-    use RefreshDatabase;
+    use RefreshDatabase, TestAuthentification;
 
     /* ------------------------------------------------------------------
      *           AUXILIAIRES : tests de controle d'acces
      * ------------------------------------------------------------------
      */
-    /**
-     * Un utilisateur non authentifie doit etre renvoye a la page de login
-     * 
-     * @dataProvider controleAccesProvider
-     */
-    public function testNonAuth(string $uriRoute)
-    {
-        $this->assertGuest()
-        ->from('/')
-        ->get(route($uriRoute))
-        ->assertRedirect(route('login'));
-    }
-
-    /**
-     * Un utilisateur qui n'est pas de la classe App\Modeles\Enseignant n'est pas autorise
-     * 
-     * @dataProvider controleAccesProvider
-     */
-    public function testNonTypeEnseignant(string $uriRoute)
-    {
-        // Creation d'un utilisateur aleatoire
-        $contact = factory(Contact::class)->create();
-        $user    = factory(User::class)->make();
-        $user->identite()->associate($contact);
-        $user->save();
-
-        // Routage echec
-        $this->actingAs($user)
-        ->from('/')
-        ->get(route($uriRoute))
-        ->assertStatus(403);
-    }
-
-    /**
-     * Un utilisateur qui n'a pas le role App\Modeles\Role::VAL_ENSEIGNANT n'est pas autorise
-     * 
-     * @dataProvider controleAccesProvider
-     */
-    public function testNonRoleEnseignant(string $uriRoute)
-    {
-        // Creation d'un enseignant aleatoire
-        $enseignant = factory(Enseignant::class)->create();
-        $user       = factory(User::class)->make();
-        $user->identite()->associate($enseignant);
-        $user->save();
-
-        // Route echec
-        $this->actingAs($user)
-        ->from('/')
-        ->get(route($uriRoute))
-        ->assertStatus(403);
-    }
 
     /**
      * Permet de facilement tester les differentes routes
