ecf09cf42deb833be0c333b5e5f5bf5319e8c831
Ikau <tlutru@gmail.com>
2020-02-13 11:15:50 +0100

    :lipstick: Alignement a droite de la navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,24 +8,23 @@
     </head>
     <body>
         @section('sidebar')
-            <nav class="navbar navbar-dark bg-primary">
-                @if (Route::has('login'))
-                    <div class="top-right links">
-                        @auth
-                            <a class="text-white" href="{{ url('/home') }}">Home</a>
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-                                <button type="submit">Logout</button>
-                            </form>
-                        @else
-                            <a class="text-white" href="{{ route('login') }}">Login</a>
+            <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+                <div class="navbar-collapse collapse justify-content-end">
+                    <div class="navbar-nav">
+                    @auth
+                    <a class="nav-item nav-link btn btn-lg text-white" href="{{ url('/home') }}">Accueil</a>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button class="btn btn-lg nav-item nav-link text-white" type="submit">Se déconnecter</button>
+                    </form>
+                    @endauth
 
-                            @if (Route::has('register'))
-                                <a class="text-white" href="{{ route('register') }}">Register</a>
-                            @endif
-                        @endauth
+                    @guest
+                        <a class="nav-item nav-link btn btn-lg text-white" href="{{ route('login') }}">Login</a>
+                        <a class="nav-item nav-link btn btn-lg text-white" href="{{ route('register') }}">Register</a>
+                    @endguest
                     </div>
-                @endif
+                </div>
             </nav>
         @show
 
