623ce4103d9bee88ffadcce5cf32c7b442618ef6
Ikau <tlutru@gmail.com>
2020-02-13 17:49:08 +0100

    :sparkles: Implementation de la route vers l'affectation du responsable

--- app/Http/Controllers/Enseignant/ResponsableController.php
+++ app/Http/Controllers/Enseignant/ResponsableController.php
@@ -1,7 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\Enseignant;
+use App\Modeles\Departement;
+use App\Modeles\Enseignant;
+use App\Modeles\Option;
 use App\Utils\Constantes;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 
 use App\Modeles\Etudiant;
@@ -20,6 +24,7 @@ class ResponsableController extends AbstractResponsableController
      * Valeur attendue du tag <title> pour les pages
      */
     const TITRE_GET_FORM_AFFECTATION = 'Responsable - Creer une affectation';
+    const TITRE_INDEX_AFFECTATION    = 'Responsable - Liste des affectations';
 
     /**
      * Indique a Lavel que toutes les fonctions de callback demandent un utilisateur
@@ -37,10 +42,61 @@ class ResponsableController extends AbstractResponsableController
     /**
      * Renvoie la page avec le formulaire de creation d'une affectation
      */
-    public function getFormAffectation()
+    public function getCreateAffectation()
     {
         Gate::authorize(Constantes::GATE_ROLE_RESPONSABLE);
 
         return redirect()->route('stages.create');
+    }
+
+    /**
+     * Renvoie la page listant toutes les affectations que le responsable peut modifier
+     */
+    public function getIndexAffectation()
+    {
+        Gate::authorize(Constantes::GATE_ROLE_RESPONSABLE);
+
+        // Recuperation du responsable courant
+        $responsable = Auth::user()->identite;
+
+        // Recuperation des stages geres par le responsable
+        $stages = [];
+        foreach(Stage::all() as $stage)
+        {
+            // Si l'enseignant est responsable du departement
+            if(Departement::VAL_AUCUN !== $responsable[Enseignant::COL_RESPONSABLE_DEPARTEMENT_ID]
+            && $stage->etudiant->departement->id === $responsable[Enseignant::COL_RESPONSABLE_DEPARTEMENT_ID])
+            {
+                $stages[] = $stage;
+            }
+
+            // Si l'enseignant est responsable de l'option
+            if(Option::VAL_AUCUN !== $responsable[Enseignant::COL_RESPONSABLE_OPTION_ID]
+                && $stage->etudiant->option->id === $responsable[Enseignant::COL_RESPONSABLE_OPTION_ID])
+            {
+                $stages[] = $stage;
+            }
+        }
+
+        // Entetes pour le tableau de la page
+        $entetes = [
+            '', // Vide pour les icones
+            'Nom stagiaire',
+            'Prenom stagiaire',
+            'Annee',
+            'Promotion',
+            'Option',
+            'Etat de l\'affectation',
+            'Etat du stage',
+            'Referent',
+            'Entreprise',
+            'Lieu'
+        ];
+
+        return view('enseignant.responsable.affectations', [
+            'titre'   => self::TITRE_INDEX_AFFECTATION,
+            'entetes' => $entetes,
+            'stages'  => $stages
+        ]);
     }
 }
