1b96865e2d1a989c0583a61121ded70aeec48fdf
Ikau <tlutru@gmail.com>
2020-02-13 10:13:04 +0100

    :lipstick: :sparkles: Affichage des notifications dans le tableau de bord

--- resources/views/enseignant/commun/index.blade.php
+++ resources/views/enseignant/commun/index.blade.php
@@ -1,27 +1,54 @@
-{{-- 
+{{--
     Page d'accueil (tableau de bord) pour l'espace commune a tous les enseignants.
 
     Variables a definir depuis la vue appelante :
         'titre'  : string   Le titre de l'onglet
-
 --}}
 @extends('layouts.app')
 
 @section('titre', $titre)
 
 @section('contenu')
-    <div>
-        <h1>Zone enseignant</h1>
-    </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-    <div>
-        Bienvenue {{Auth::user()->identite->prenom}} {{Auth::user()->identite->nom}}
-    </div>
-    <div>
-        <a href="{{ route('referents.affectations') }}">Mes affectations</a>
+                <div class="card-header text-white bg-secondary">
+                    <h3>Liens utiles</h3>
+                </div>
+                <div class="card-body">
+                    <a class="btn btn-lg btn-success" href="{{ route('referents.affectations') }}">🎓 Mes affectations</a>
 
-        @if( Auth::user()->estResponsableOption() || Auth::user()->estResponsableDepartement() )
-        <a href="{{ route('responsables.affectations.get') }}">Proposer une affectation</a>
-        @endif
+                    @if( Auth::user()->estResponsableOption() || Auth::user()->estResponsableDepartement() )
+                        <a class="btn btn-lg btn-primary" href="{{ route('responsables.affectations.get') }}">➕ Proposer une affectation</a>
+                    @endif
+                </div>
+
+                <div class="card-header bg-primary text-white">
+                    <p>Bienvenue {{ Auth::user()->identite->prenom }} {{ Auth::user()->identite->nom }}</p>
+                    <h3>Mes derniers évènements</h3>
+                </div>
+
+                <div class="card-body">
+                    @foreach(Auth::user()->notifications()->limit(5)->get() as $notification)
+                        <div class="card-header border h4">
+                            <span class="badge badge-secondary">{{ $notification->created_at->format('d-m-yy') }}</span>
+                            <span class="badge badge-info text-white">{{ \App\Facade\NotificationFacade::getTypeEvenement($notification) }}</span>
+                            <span>{{ \App\Facade\NotificationFacade::getTitre($notification) }}</span>
+                        </div>
+                        <div class="card-body border">
+                            {!! \App\Facade\NotificationFacade::getMessage($notification) !!}
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,25 @@
+{{--
+    Affichage de la page d'accueil de l'espace enseignant
+
+    Variables a definir depuis la vue appelante :
+            'titre'         => string                               Le titre de la page
+            'notifications' => Colleciton(InformationsNotification) Les notifications recents implementant l'interface InformationsNotifications
+--}}
 @extends('layouts.app')
 
 @section('contenu')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header bg-primary text-white">
+                    <p>Bienvenue {{ Auth::user()->identite->prenom }} {{ Auth::user()->identite->nom }}</p>
+                </div>
+                <div class="card-header text-white bg-secondary">
+                    <h3>🏗️ Liens utiles 🚧</h3>
+                </div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                </div>
-
-                </div>
-                    Bienvenue {{ $identite->prenom }} {{ $identite->nom }}
-                </div>
-                <div>
-                    Liens de test
-                    <div>
-                        <a href="{{ route('referents.index') }}">Espace enseignant</a>
-                    </div>
+                    <a class="btn btn-lg btn-primary" href="{{ route('referents.index') }}">Espace enseignant</a>
                 </div>
             </div>
         </div>
