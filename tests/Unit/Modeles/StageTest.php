0c370ba61f3795b2e727a9831e1c5d1f653a5e12
Ikau <tlutru@gmail.com>
2020-02-13 17:47:47 +0100

    :sparkles: Champ 'affectation_validee' dans modele 'Stage'

--- tests/Unit/Modeles/StageTest.php
+++ tests/Unit/Modeles/StageTest.php
@@ -25,17 +25,18 @@ class StageTest extends TestCase
         // Copier/Coller des attributs 'protected' du modele
         $attributsTests = [
             // Attributs propres au modele
-            Stage::COL_ANNEE              => Constantes::INT_VIDE,
-            Stage::COL_CONVENTION_ENVOYEE => FALSE,
-            Stage::COL_CONVENTION_SIGNEE  => FALSE,
-            Stage::COL_DATE_DEBUT         => Constantes::DATE_VIDE,
-            Stage::COL_DATE_FIN           => Constantes::DATE_VIDE,
-            Stage::COL_DUREE_SEMAINES     => Constantes::INT_VIDE,
-            Stage::COL_GRATIFICATION      => Constantes::FLOAT_VIDE,
-            Stage::COL_INTITULE           => Constantes::STRING_VIDE,
-            Stage::COL_LIEU               => Constantes::STRING_VIDE,
-            Stage::COL_MOYEN_RECHERCHE    => Constantes::STRING_VIDE,
-            Stage::COL_RESUME             => Constantes::STRING_VIDE,
+            Stage::COL_AFFECTATION_VALIDEE => FALSE,
+            Stage::COL_ANNEE               => Constantes::INT_VIDE,
+            Stage::COL_CONVENTION_ENVOYEE  => FALSE,
+            Stage::COL_CONVENTION_SIGNEE   => FALSE,
+            Stage::COL_DATE_DEBUT          => Constantes::DATE_VIDE,
+            Stage::COL_DATE_FIN            => Constantes::DATE_VIDE,
+            Stage::COL_DUREE_SEMAINES      => Constantes::INT_VIDE,
+            Stage::COL_GRATIFICATION       => Constantes::FLOAT_VIDE,
+            Stage::COL_INTITULE            => Constantes::STRING_VIDE,
+            Stage::COL_LIEU                => Constantes::STRING_VIDE,
+            Stage::COL_MOYEN_RECHERCHE     => Constantes::STRING_VIDE,
+            Stage::COL_RESUME              => Constantes::STRING_VIDE,
 
             // Clefs etrangeres
             Stage::COL_REFERENT_ID   => Constantes::ID_VIDE,
