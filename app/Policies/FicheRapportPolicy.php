172be0bc327d9ebf9bcc7f94e845df12334f3e76
Ikau <tlutru@gmail.com>
2020-02-19 18:09:50 +0100

    :lock::sparkles: Controle d'acces pour l'edition d'une fiche de rapport

--- app/Policies/FicheRapportPolicy.php
+++ app/Policies/FicheRapportPolicy.php
@@ -11,11 +11,29 @@ class FicheRapportPolicy
     use HandlesAuthorization;
 
     /**
+     * Indique si un compte utilisateur a le droit de voir une fiche de rapport
+     *
      * @param User $user
      * @param FicheRapport $ficheRapport
      * @return bool
      */
     public function show(User $user, FicheRapport $ficheRapport)
+    {
+
+        return (
+            ($user->identite->id === $ficheRapport->stage->referent->id)
+            || $user->estScolariteINSA()
+        );
+    }
+
+    /**
+     * Indique si un compte utilisateur a le droit de modifier une fiche de rapport
+     *
+     * @param User $user
+     * @param FicheRapport $ficheRapport
+     * @return bool
+     */
+    public function edit(User $user, FicheRapport $ficheRapport)
     {
         return $user->identite->id === $ficheRapport->stage->referent->id;
     }
