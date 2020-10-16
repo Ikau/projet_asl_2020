71ca84e8ebd648680d18a0e27249f57378430286
Ikau <tlutru@gmail.com>
2020-10-16 14:29:58 +0200

    🎨 Ajoute suppression cascade pour table pivot

--- database/migrations/2020_01_16_164909_creer_table_role_user.php
+++ database/migrations/2020_01_16_164909_creer_table_role_user.php
@@ -24,8 +24,8 @@ class CreerTableRoleUser extends Migration
             $table->unsignedBigInteger(User::COL_PIVOT);
             $table->unsignedBigInteger(Role::COL_PIVOT);
 
-            $table->foreign(User::COL_PIVOT)->references('id')->on(User::NOM_TABLE);
-            $table->foreign(Role::COL_PIVOT)->references('id')->on(Role::NOM_TABLE);
+            $table->foreign(User::COL_PIVOT)->references('id')->on(User::NOM_TABLE)->onDelete('cascade');
+            $table->foreign(Role::COL_PIVOT)->references('id')->on(Role::NOM_TABLE)->onDelete('cascade');
 
             // On indique que la combinaison est unique
             $table->unique([User::COL_PIVOT, Role::COL_PIVOT]);
